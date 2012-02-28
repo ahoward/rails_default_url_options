8438f640a55c19daef43d42f4a479b265a22ef0d
ara.t.howard <ara.t.howard@gmail.com>
2012-02-28 15:34:02 -0700

    for rails 3.2.1

--- lib/rails_default_url_options.rb
+++ lib/rails_default_url_options.rb
@@ -31,7 +31,7 @@ unless defined?(DefaultUrlOptions)
   DefaultUrlOptions = defined?(HashWithIndifferentAccess) ? HashWithIndifferentAccess.new : Hash.new
 
   def DefaultUrlOptions.version
-    '1.3.1'
+    '1.3.2'
   end
 
   def DefaultUrlOptions.configure(request = {})
